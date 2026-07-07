121d68a055aedea721a6d2b58b8ce61ef179e995
Nurlan2121888 <nurlanhuseynli911@gmail.com>
2026-07-07 20:51:17 +0400

    2ci

---
+++ ders2.py
@@ -0,0 +1,73 @@
+# if elif else
+# number = 5  
+
+# if number > 10:
+#     print("eded 10 dan boyukdur")
+# elif number > 5:
+#     print("eded 5-den boyukdur")    
+# else:
+#     print("hec bir sert odenmir")     ```
+
+
+# yas=20  
+
+# if yas >= 18:
+#     print("yetkin")
+# else:
+#     print("yetkin deyil")
+    
+# bal=49
+    
+# if bal >= 90:
+#         print("A")  
+# elif bal >= 70:
+#             print("B")
+# elif bal >= 50:
+#                 print("C") 
+# elif bal < 50:
+#                     print("Kesildi")                            
+# name= "Samil"
+
+# if"n" in name:
+#     print("sert odenir")
+# else:
+#     print("sert odenmir")    
+    
+#     city="Baku"
+    
+#     if "z" not in city:
+#         print("sert odenir")    
+#     else:
+#         print("sert odenmir")  
+
+# city="London"   
+
+# if "r" in city or "b" in city or "a" in city:
+#     print("sert odenir")
+# else:
+#     print("sert odenmir")    
+    
+# name="Ayan" 
+    
+# if "y" in name and "a" in name:
+#         print("sert odenir")
+# else:
+#         print("sert odenmir")       
+
+# number = int(input("Ededi daxil edin :"))
+
+# if number > 0:
+#     print("Eded musbetdir ededdir")
+# elif number < 0:
+#     print("Eded menfidir ededdir")
+# else:
+#     print("bu eded sifirdir")
+
+
+number1 = int(input("Ededi daxil edin :"))
+number2 = int(input("Ededi daxil edin :"))
+
+# # print("Maximum eded:", max(number1, number2))  
+# print("Minimum eded:", min(number1, number2))   
+
+print(pow(number1, number2))
