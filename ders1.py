264e4cb3a51cfa20330b6d2d54fda3040763f91d
Nurlan2121888 <nurlanhuseynli911@gmail.com>
2026-07-07 19:51:36 +0400

    python ders 1 test

---
+++ ders1.py
@@ -0,0 +1,69 @@
+# print("Hello World!")
+
+
+# x=5
+
+# x=10
+
+# print(x)
+
+# str()
+# x="Python"
+
+# int()
+# number=5
+
+# float()
+# float_number=2.5
+
+# bool()
+# bool_number=True
+
+# print(type(bool_number))
+# print(type(number))
+
+
+from curses import version
+
+
+prog_lang="java" 
+
+# print(prog_lang[0])
+
+# print(prog_lang.count("a"))
+
+# print(prog_lang.capitalize())
+
+# news="you must enable javascript"
+
+
+# city="Baku"
+
+# print(city.upper())
+# print(city.lower())
+
+# name=input("Adinizi daxil edin :")
+
+# print("Hello"+" "+name)
+# number1=5
+# number2=10
+# print(number1+number2)
+# city="Baku"
+
+# name="Nurlan"
+
+# print(f"{name} {city} seherinde yasayir")
+
+# print(5/2)
+# print(5//2)
+# print(5%2)
+# print(5!=2)
+yas=22
+print(yas>18 and yas<30)
+print(not yas<30)
+git --global user.username "Nurlan2121888"
+git --version
+git --version
+
+
+      
